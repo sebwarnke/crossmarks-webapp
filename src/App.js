0ac7e0aaf313460465ae3a830f560f37e953c986
Sebastian Warnke <sw-github@sebwarnke.com>
2020-07-28 22:32:41 +0200

    query bookmark api and render results

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    bookmarks: []
+  }
+
+  async componentDidMount() {
+    const response = await fetch('/api/bookmark');
+    const body = await response.json();
+    this.setState({
+      bookmarks: body,
+      isLoading: false
+    });
+  }
+
+
+
+
+  render() {
+    const { bookmarks, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>Crossmarks List</h2>
+            {bookmarks.map(bookmark =>
+              <div key={bookmark.id}>
+                {bookmark.name}
+              </div>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
